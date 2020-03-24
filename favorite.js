38d9de7d98fd58b55a6cf0c1721d743d03aff171
victoriasun <victoriasun610@hotmail.com>
2020-03-23 20:49:12 -0700

    add all 4 files

---
+++ favorite.js
@@ -0,0 +1,151 @@
+(function () {
+    const BASE_URL = 'https://movie-list.alphacamp.io'
+    const INDEX_URL = BASE_URL + '/api/v1/movies/'
+    const POSTER_URL = BASE_URL + '/posters/'
+    const data = []
+    const searchForm = document.getElementById('search')
+    const searchInput = document.getElementById('search-input')
+    const pagination = document.getElementById('pagination')
+    const ITEM_PER_PAGE = 12
+    let paginationData=[]
+
+
+
+    searchForm.addEventListener('submit', event => {
+    event.preventDefault()
+    let input = searchInput.value.toLowerCase()
+    let results = data.filter(movie => movie.title.toLowerCase().includes(input))
+    console.log(results)
+    getTotalPages(results)
+    getPageData(results)
+  
+    })
+  
+    const dataPanel = document.getElementById('data-panel')
+    dataPanel.addEventListener('click', (event) => {
+        if(event.target.matches('.btn-show-movie')) {
+            showMovie(event.target.dataset.id)
+        } else if(event.target.matches('.btn-remove-favorite')) {
+            removeFavoriteItem(event.target.dataset.id)
+        }
+    })    
+
+
+    axios.get(INDEX_URL).then((response) => {
+        data.push(...response.data.results)
+        getTotalPages(data)  
+        getPageData(1, data)    // add here
+      }).catch((err) => console.log(err))
+
+
+    displayDataList(results)
+
+})
+
+pagination.addEventListener('click', event => {
+    console.log(event.target.dataset.page)
+    if (event.target.tagName === 'A') {
+      getPageData(event.target.dataset.page)
+    }
+})
+  
+
+  
+    function getTotalPages (data) {
+      let totalPages = Math.ceil(data.length / ITEM_PER_PAGE) || 1
+      let pageItemContent = ''
+      for (let i = 0; i < totalPages; i++) {
+        pageItemContent += `
+          <li class="page-item">
+            <a class="page-link" href="javascript:;" data-page="${i + 1}">${i + 1}</a>
+          </li>
+        `
+      }
+      pagination.innerHTML = pageItemContent
+    }
+
+    function getPageData(pageNum, data) {
+        pagenationData = data || pagenationData
+        let offset = (pageNum - 1) * ITEM_PER_PAGE
+        let pageData = paginationData.slice(offset, offset + ITEM_PER_PAGE)
+        displayDataList(pageData)
+    }
+
+      
+
+    
+  
+    function displayDataList (data) {
+      let htmlContent = ''
+      data.forEach(function (item, index) {
+        htmlContent += `
+          <div class="col-sm-3">
+            <div class="card mb-2">
+              <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+              <div class="card-body movie-item-body">
+                <h6 class="card-title">${item.title}</h6>
+              </div>
+
+              <!-- "More" button -->
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal"data-id ="${item.id}">More</button>
+              <button class="btn btn-info btn-remove-favorite" data-id="${item.id}">X</button>
+            </div>
+          </div>
+         </div>
+        `
+      })
+      dataPanel.innerHTML = htmlContent
+    }
+
+    // function addFavoriteItem (id) {
+    //     const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+    //     const movie = data.find(item => item.id === Number(id))
+      
+    //     if (list.some(item => item.id === Number(id))) {
+    //       alert(`${movie.title} is already in your favorite list.`)
+    //     } else {
+    //       list.push(movie)
+    //       alert(`Added ${movie.title} to your favorite list!`)
+    //     }
+    //     localStorage.setItem('favoriteMovies', JSON.stringify(list))
+    //   }
+
+
+    function removeFavoriteItem(id) {
+        const index = data.findIndex(item => item.id === Number(id))
+        if(index.id === -1) return
+
+        data.splice(index, 1)
+        localStorage.setItem('favoriteMovies', JSON.stringify(data))
+
+        displayDataList(data)
+    }
+
+    function showMovie (id) {
+        // get elements
+        const modalTitle = document.getElementById('show-movie-title')
+        const modalImage = document.getElementById('show-movie-image')
+        const modalDate = document.getElementById('show-movie-date')
+        const modalDescription = document.getElementById('show-movie-description')
+    
+        // set request url
+        const url = INDEX_URL + id
+        console.log(url)
+    
+        // send request to show api
+        axios.get(url).then(response => {
+          const data = response.data.results
+          console.log(data)
+    
+          // insert data into modal ui
+          modalTitle.textContent = data.title
+          modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+          modalDate.textContent = `release at : ${data.release_date}`
+          modalDescription.textContent = `${data.description}`
+        })
+      }
+
+  
+    
+
---
+++ index.js
@@ -0,0 +1,107 @@
+(function () {
+    const BASE_URL = 'https://movie-list.alphacamp.io'
+    const INDEX_URL = BASE_URL + '/api/v1/movies/'
+    const POSTER_URL = BASE_URL + '/posters/'
+    const data = []
+    const searchForm = document.getElementById('search')
+    const searchInput = document.getElementById('search-input')
+
+
+    searchForm.addEventListener('submit', event => {
+    event.preventDefault()
+    let input = searchInput.value.toLowerCase()
+    let results = data.filter(movie => movie.title.toLowerCase().includes(input))
+    console.log(results)
+    displayDataList(results)
+    })
+  
+    const dataPanel = document.getElementById('data-panel')
+    dataPanel.addEventListener('click', (event) => {
+        if(event.target.matches('.btn-show-movie')) {
+            showMovie(event.target.dataset.id)
+        }
+    })    
+  
+    axios.get(INDEX_URL).then((response) => {
+      data.push(...response.data.results)
+      console.log(data)
+      displayDataList(data)
+    }).catch((err) => console.log(err))
+  
+    function displayDataList (data) {
+      let htmlContent = ''
+      data.forEach(function (item, index) {
+        htmlContent += `
+          <div class="col-sm-3">
+            <div class="card mb-2">
+              <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+              <div class="card-body movie-item-body">
+                <h6 class="card-title">${item.title}</h6>
+              </div>
+
+              <!-- "More" button -->
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal"data-id ="${item.id}">More</button>
+            </div>
+          </div>
+         </div>
+        `
+      })
+      dataPanel.innerHTML = htmlContent
+    }
+
+    function showMovie (id) {
+        // get elements
+        const modalTitle = document.getElementById('show-movie-title')
+        const modalImage = document.getElementById('show-movie-image')
+        const modalDate = document.getElementById('show-movie-date')
+        const modalDescription = document.getElementById('show-movie-description')
+    
+        // set request url
+        const url = INDEX_URL + id
+        console.log(url)
+    
+        // send request to show api
+        axios.get(url).then(response => {
+          const data = response.data.results
+          console.log(data)
+    
+          // insert data into modal ui
+          modalTitle.textContent = data.title
+          modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+          modalDate.textContent = `release at : ${data.release_date}`
+          modalDescription.textContent = `${data.description}`
+        })
+      }
+
+  })()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+  
+    
+    
+    
